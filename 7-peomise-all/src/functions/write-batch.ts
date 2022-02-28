114cbcb8d8b2f94206d7c64442cf8b1a625694f2
CircleCI <no-reply@build.com>
2022-02-28 12:03:24 +0200

    functions

--- 7-peomise-all/src/functions/write-batch.ts
+++ 7-peomise-all/src/functions/write-batch.ts
@@ -1,13 +1,17 @@
 import {initSecret} from "../lib/SSM";
 import * as DynamoDB from "../lib/DynamoDB";
 
-export const handler = async (items) => {
-    const secret = await initSecret('top-secret','TOP_SECRET');
-    if(secret === "top-secret message"){
-        await Promise.all(items.map(async(item)=>DynamoDB.createItem(process.env.USERS_TABLE!,item)));
-        console.log("success")
-    } else{
-        console.log("bad secret")
-    }
+const promise = initSecret('top-secret','TOP_SECRET');
 
+export const handler = async (items) => {
+    console.log("items: ", items);
+    return promise.then(async (secret)=>{
+        if(secret === "top-secret message"){
+            await Promise.all(items.map(async(item)=>DynamoDB.createItem(process.env.USERS_TABLE!,item)));
+            console.log("success")
+        } else{
+            console.log("bad secret")
+        }
+
+    });
 };
--- 8-dynamo-batch/src/cold-starts.ts
+++ 8-dynamo-batch/src/cold-starts.ts
@@ -15,21 +15,21 @@ for(let i = 0 ; i < 100 ; i++){
         let value = (Math.random() + 1).toString(36).substring(7);
         await new Promise((resolve) => setTimeout(resolve, 1000));
         await Promise.all([
-            // invokeLambda("lambdaborghini-2-naive-write-batch", payload, "Event"),
+            invokeLambda("lambdaborghini-2-naive-write-batch", payload, "Event"),
             // invokeLambda("lambdaborghini-3-layers-plugin-write-batch", payload, "Event"),
-            // invokeLambda("lambdaborghini-3-layers-plugin-split-write-batch", payload, "Event"),
-            invokeLambda("lambdaborghini-5-optimized-imports-write-batch", payload, "Event"),
-            // invokeLambda("lambdaborghini-6-webpack-write-batch", payload, "Event"),
+            invokeLambda("lambdaborghini-3-layers-plugin-split-write-batch", payload, "Event"),
+            invokeLambda("lambdaborghini-4-optimized-imports-write-batch", payload, "Event"),
+            // invokeLambda("lambdaborghini-9-webpack-write-batch", payload, "Event"),
+            invokeLambda("lambdaborghini-6-optimized-init-write-batch", payload, "Event"),
             invokeLambda("lambdaborghini-7-peomise-all-write-batch", payload, "Event"),
-            invokeLambda("lambdaborghini-8-optimized-init-write-batch", payload, "Event"),
-            invokeLambda("lambdaborghini-9-dynamo-batch-write-batch", payload, "Event")
+            invokeLambda("lambdaborghini-8-dynamo-batch-write-batch", payload, "Event")
         ]);
         // await Promise.all([
         //     updateConf("lambdaborghini-2-naive-write-batch", name, value),
         //     updateConf("lambdaborghini-3-layers-plugin-write-batch", name, value),
         //     updateConf("lambdaborghini-3-layers-plugin-split-write-batch", name, value),
-        //     updateConf("lambdaborghini-5-optimized-imports-write-batch", name, value),
-        //     updateConf("lambdaborghini-6-webpack-write-batch", name, value)
+        //     updateConf("lambdaborghini-4-optimized-imports-write-batch", name, value),
+        //     updateConf("lambdaborghini-9-webpack-write-batch", name, value)
         // ]);
     }
 
